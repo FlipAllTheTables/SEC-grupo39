d7edb7e1192fd8e8c0a5eca3fe0af338be351ecb
FlipAllTheTables <rafaelbgwr@gmail.com>
2025-04-06 14:13:35 +0100

    Created KeysUtil class with static ReadRSA method

---
+++ DepChain/src/main/java/com/ist/DepChain/util/KeysUtil.java
@@ -0,0 +1,32 @@
+package com.ist.DepChain.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+public class KeysUtil {
+
+    private KeysUtil() {}
+
+    public static Key readRSA(String keyPath, String type) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] encoded;
+        try (FileInputStream fis = new FileInputStream(keyPath)) {
+            encoded = new byte[fis.available()];
+            fis.read(encoded);
+        }
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        if (type.equals("pub") ){
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+            return keyFactory.generatePublic(keySpec);
+        }
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+        return keyFactory.generatePrivate(keySpec);
+    }
+    
+}
